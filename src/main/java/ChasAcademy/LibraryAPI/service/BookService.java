eb43719a53649657082c33240b87b57d76add94e
Joso1207 <joso1207@student.miun.se>
2026-03-14 14:50:42 +0100

    Service uses DTO Mapper and tosses response exceptions

--- src/main/java/ChasAcademy/LibraryAPI/service/BookService.java
+++ src/main/java/ChasAcademy/LibraryAPI/service/BookService.java
@@ -1,34 +1,48 @@
 package ChasAcademy.LibraryAPI.service;
 
 import ChasAcademy.LibraryAPI.api.dto.BookRequestDTO;
+import ChasAcademy.LibraryAPI.api.dto.NewBookRequestDTO;
+import ChasAcademy.LibraryAPI.api.mapper.Mapper;
 import ChasAcademy.LibraryAPI.persistence.model.Book;
 import ChasAcademy.LibraryAPI.persistence.repository.BookRepository;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class BookService {
 
     private final BookRepository repo;
+    private final Mapper mapper;
 
     @Autowired
-    public BookService(BookRepository repo){
+    public BookService(BookRepository repo, Mapper mapper){
+
         this.repo = repo;
+        this.mapper = mapper;
+
+    }
+
+    public List<BookRequestDTO> findAll(){
+        return repo.findAll()
+                .stream()
+                .map(mapper::toBookDTO)
+                .toList();
     }
 
     public @NonNull BookRequestDTO getBookByID(Long id){
-       Book entity = repo.findById(id).orElseThrow();
+       Book entity = repo.findById(id).orElseThrow(
+               () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found"));
+       return mapper.toBookDTO(entity);
+    }
 
-       return BookRequestDTO.builder()
-               .id(entity.getId())
-               .title(entity.getTitle())
-               .isbn((entity.getIsbn()))
-               .author(entity.getAuthor())
-               .yearPublished(entity.getPublishedYear())
-               .build();
+    public BookRequestDTO save(NewBookRequestDTO dto){
+        Book newBook = mapper.dtoToBook(dto);
+        return mapper.toBookDTO(repo.save(newBook));
     }
 
 

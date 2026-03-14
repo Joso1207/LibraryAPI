3f9ff3a412f6dd16c61951b0588a10f320d4392a
Joso1207 <joso1207@student.miun.se>
2026-03-14 14:51:10 +0100

    Updated controller with post and new api mapping

--- src/main/java/ChasAcademy/LibraryAPI/api/controller/BookController.java
+++ src/main/java/ChasAcademy/LibraryAPI/api/controller/BookController.java
@@ -1,15 +1,16 @@
 package ChasAcademy.LibraryAPI.api.controller;
 
 import ChasAcademy.LibraryAPI.api.dto.BookRequestDTO;
+import ChasAcademy.LibraryAPI.api.dto.NewBookRequestDTO;
 import ChasAcademy.LibraryAPI.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/v2/api/books")
 public class BookController {
 
     private final BookService service;
@@ -18,10 +19,22 @@ public class BookController {
         this.service=service;
     }
 
+
+    @PostMapping
+    public ResponseEntity<BookRequestDTO> addBook(@RequestBody NewBookRequestDTO newBook){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(newBook));
+    }
+
+    @GetMapping
+    public List<BookRequestDTO> getAll(){
+        return service.findAll();
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<BookRequestDTO> getBookByID(@PathVariable Long id){
         return ResponseEntity.ok(service.getBookByID(id));
     }
+
 
 
 }

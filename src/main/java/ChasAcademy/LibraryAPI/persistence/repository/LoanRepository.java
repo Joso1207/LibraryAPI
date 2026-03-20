af1e3f0a54e8042eb35b6abd7e3961fa7ef10999
Joso1207 <joso1207@student.miun.se>
2026-03-20 18:32:40 +0100

    Loan Repository

---
+++ src/main/java/ChasAcademy/LibraryAPI/persistence/repository/LoanRepository.java
@@ -0,0 +1,14 @@
+package ChasAcademy.LibraryAPI.persistence.repository;
+
+import ChasAcademy.LibraryAPI.persistence.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LoanRepository extends JpaRepository<Loan,Long> {
+
+    Optional<Loan> findByBookId(Long id);
+    List<Loan> findByReturnDateIsNull();
+
+}

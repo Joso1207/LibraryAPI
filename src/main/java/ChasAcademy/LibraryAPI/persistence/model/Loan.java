03cccff1147f5c9dc14be0496b80cbbc871ea935
Joso1207 <joso1207@student.miun.se>
2026-03-20 18:26:45 +0100

    Loan entity

---
+++ src/main/java/ChasAcademy/LibraryAPI/persistence/model/Loan.java
@@ -0,0 +1,31 @@
+package ChasAcademy.LibraryAPI.persistence.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    @OneToOne
+    @NotNull
+    @JoinColumn(name = "book_id", nullable = false, unique = true)
+    Book book;
+
+    @Column
+    LocalDate loanDate = LocalDate.now();
+
+    @Column
+    LocalDate returnDate = null;
+
+}

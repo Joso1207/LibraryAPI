6fd087148399332c2d47fde065e944e2b7c83a1f
Joso1207 <joso1207@student.miun.se>
2026-04-16 22:56:32 +0200

    cleanup

--- src/main/java/ChasAcademy/LibraryAPI/persistence/model/Book.java
+++ src/main/java/ChasAcademy/LibraryAPI/persistence/model/Book.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.ISBN;
+import org.springframework.data.repository.cdi.Eager;
 
 @Entity
 @Builder

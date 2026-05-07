8e715343869b34953913b8e2d1b0228e2ed5c7fd
Joso1207 <joso1207@student.miun.se>
2026-05-07 23:45:32 +0200

    Spring Vault

--- src/main/java/ChasAcademy/LibraryAPI/service/BookService.java
+++ src/main/java/ChasAcademy/LibraryAPI/service/BookService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
